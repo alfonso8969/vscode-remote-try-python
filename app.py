4cae94195936cdf46fe31473ea3932dd4648ffa2
Alfonso J. González <25032083+alfonso8969@users.noreply.github.com>
2023-12-02 19:08:45 +0000

    init commit

--- app.py
+++ app.py
@@ -4,8 +4,33 @@
 #-----------------------------------------------------------------------------------------
 
 from flask import Flask
+import random
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+
+def play_game():
+    choices = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(choices)
+    user_choice = input("Enter your choice (rock/paper/scissors): ").lower()
+
+    print(f"Computer chooses: {computer_choice}")
+    print(f"You choose: {user_choice}")
+
+    if user_choice not in choices:
+        print("Invalid choice. Please try again.")
+        return
+
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif (user_choice == "rock" and computer_choice == "scissors") or \
+            (user_choice == "paper" and computer_choice == "rock") or \
+            (user_choice == "scissors" and computer_choice == "paper"):
+        print("You win!")
+    else:
+        print("Computer wins!")
+
+play_game()
